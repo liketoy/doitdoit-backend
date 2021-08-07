25deadc947b36e6d6e9636fe2d8752b8d4eeb88d
Dongseon Lee <eastline@maycan.co.kr>
2021-08-07 16:42:38 +0900

    Initial Commit

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class CoreModel(models.Model):
+
+    """Time Stamped Model Definition"""
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
